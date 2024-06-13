baddd6eff83a8796575acdaeff732e93b4f15f55
Mohit Maurya <74113320+mohitmaurya2023@users.noreply.github.com>
2024-06-13 16:37:22 +0530

    new dataframe

--- Streamlit/first_app.py
+++ Streamlit/first_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
