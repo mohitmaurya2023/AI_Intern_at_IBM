da36906c65932688ea75956fb6cca66c447c75e6
Mohit Maurya <74113320+mohitmaurya2023@users.noreply.github.com>
2024-06-13 17:13:49 +0530

    Use a selectbox for options

--- Streamlit/first_app.py
+++ Streamlit/first_app.py
@@ -46,3 +46,16 @@ if st.checkbox('Show dataframe'):
     )
 
     chart_data
+
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+option = st.selectbox(
+    'Which number do you like best?',
+    df['first column']
+)
+
+'You selected: ', option
