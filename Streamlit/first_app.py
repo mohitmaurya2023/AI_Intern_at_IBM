49156943137e42f4aa0eef8a21e8c9851fa73811
Mohit Maurya <74113320+mohitmaurya2023@users.noreply.github.com>
2024-06-13 16:46:44 +0530

    use styler object

--- Streamlit/first_app.py
+++ Streamlit/first_app.py
@@ -7,5 +7,12 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
+# dataframe = np.random.randn(10, 20)
+# st.dataframe(dataframe)
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20))
+)
+
+st.dataframe(dataframe.style.highlight_max(axis = 0))
