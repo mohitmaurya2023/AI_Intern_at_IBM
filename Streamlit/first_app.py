71433acd258bfb31fad4699e0aa043efce897a94
Mohit Maurya <74113320+mohitmaurya2023@users.noreply.github.com>
2024-06-19 11:03:50 +0530

    import numpy

--- Streamlit/first_app.py
+++ Streamlit/first_app.py
@@ -59,3 +59,15 @@ option = st.selectbox(
 )
 
 'You selected: ', option
+
+# Add a selectbox to the slidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
