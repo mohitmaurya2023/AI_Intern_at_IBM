49ad995738fd7650bd1ed641d65280a7ad0b6d04
Mohit Maurya <74113320+mohitmaurya2023@users.noreply.github.com>
2024-07-16 11:02:29 +0530

    Update practice_streamlit.py

--- Streamlit/practice_streamlit.py
+++ Streamlit/practice_streamlit.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 
 st.text('Fixed width text')
+st.markdown('_MARKDOWN_')
+st.caption('Balloons. Hundreds of them...')
+st.latex(r\'\'\ e^{i\pi} + 1 = 0 \'\'\')
+st.write('Most objects')
+st.write(['st', 'is <', 3])
+st.title('My title')
+st.header('My Header')
+         st.subheader('My sub')
+         st.code('for i in range(8): foo()')
