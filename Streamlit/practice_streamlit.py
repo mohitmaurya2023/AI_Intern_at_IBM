ada2f20445f5f3e3ca471fa7f6daa5839a8682cc
Mohit Maurya <74113320+mohitmaurya2023@users.noreply.github.com>
2024-07-16 11:36:52 +0530

    update

--- Streamlit/practice_streamlit.py
+++ Streamlit/practice_streamlit.py
@@ -1,13 +1,27 @@
 import streamlit as st
+st.title('Display text')
 
 
 st.text('Fixed width text')
 st.markdown('_MARKDOWN_')
 st.caption('Balloons. Hundreds of them...')
-# st.latex(r\'\'\ e^{i\pi} + 1 = 0 \'\'\')
+st.latex( r \'\'\' e^{i\pi} + 1 = 0 \'\'\')
 st.write('Most objects')
 st.write(['st', 'is <', 3])
-st.title('My title')
 st.header('My Header')
 st.subheader('My sub')
 st.code('for i in range(8): foo()')
+
+
+st.title('Display data')
+
+st.dataframe(my_dataframe)
+st.table(data.iloc[0:10])
+st.json({'foo': 'bar', 'fu':'ba'})
+st.metric(label="Temp", value = "273 K", delta = '1.2 K')
+
+st.title('Display media')
+
+st.image('./header.png')
+st.audio(data)
+st.video(data)
